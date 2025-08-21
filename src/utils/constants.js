7340112e420291daf04d3820061752430bede1c8
Shivam barthwal <shivam.paulbarthwal@gmail.com>
2025-08-21 10:09:49 +0530

    url updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "/api";
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "/api";
+// export const API_BASE_URL = "http://localhost:4000";
